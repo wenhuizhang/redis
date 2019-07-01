2267ba04fad08f5445ab3607dab807975b2a58f3
wenhuizhang <wenhui@gwmail.gwu.edu>
2019-06-30 20:25:12 -0400

    add src

--- get-redis.sh
+++ get-redis.sh
@@ -5,5 +5,8 @@ set -m
 wget http://download.redis.io/releases/redis-5.0.5.tar.gz
 tar xzf redis-5.0.5.tar.gz
 rm redis-5.0.5.tar.gz
-cd redis-5.0.5
+rm redis-5.0.5/deps/update-jemalloc.sh
+cp update-jemalloc.sh redis-5.0.5/deps/update-jemalloc.sh
+cd redis-5.0.5/deps/
+./update-jemalloc.sh
 
